3b8947f5ed230c897a65a004bdcd15f0c9716a25
faculuns <facundoluns430@gmail.com>
2026-07-01 20:27:50 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,9 +5,10 @@
 // Instrucciones completas en README.md
 // ─────────────────────────────────────────────────────────
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "cuaderno-de-nosotros-xxxx.firebaseapp.com",
-  projectId: "cuaderno-de-nosotros-xxxx",
-  storageBucket: "cuaderno-de-nosotros-xxxx.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+  apiKey: "AIzaSyDM8QeO9Ob40bgHLqgDkN7j8ZnF-RSXgRM",
+  authDomain: "recordatorios-68934.firebaseapp.com",
+  projectId: "recordatorios-68934",
+  storageBucket: "recordatorios-68934.firebasestorage.app",
+  messagingSenderId: "686047817464",
+  appId: "1:686047817464:web:6219a4652d5e366856550d"
+};
